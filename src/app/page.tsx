3c67c17be8765b8dc65d553c637cdff78efd9c97
Gutinnho <glunardi99@gmail.com>
2023-08-09 22:37:04 -0300

    feat: Defined the foundation for the beginning of development
    Files and initial structure for the project have been set. This configuration provides a solid foundation for future development

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<main className='h-full w-full flex items-center justify-center'>
+			<div>
+				<p className='text-2xl text-slate-50 font-bold'>Hello World!</p>
+			</div>
+		</main>
+	);
+}
